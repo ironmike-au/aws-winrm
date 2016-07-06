ae382d82a03644ce247220bf89356ccfbf4e7c0e
Michael Richardson <mail@mikerichardson.com.au>
2016-07-06 21:03:56 +1000

    Initial

---
+++ win-scripts/ec2-reset-admin-password.ps1
@@ -0,0 +1,13 @@
+$EC2SettingsFile="C:\\Program Files\\Amazon\\Ec2ConfigService\\Settings\\Config.xml"
+$xml = [xml](get-content $EC2SettingsFile)
+$xmlElement = $xml.get_DocumentElement()
+$xmlElementToModify = $xmlElement.Plugins
+
+foreach ($element in $xmlElementToModify.Plugin)
+{
+    if ($element.name -eq "Ec2SetPassword")
+    {
+        $element.State="Enabled"
+    }
+}
+$xml.Save($EC2SettingsFile)
---
+++ win-scripts/set-region.ps1
@@ -0,0 +1,4 @@
+Set-WinHomeLocation 12
+Set-Culture en-AU
+set-winsystemlocale en-AU
+tzutil /s "AUS Eastern Standard Time"
